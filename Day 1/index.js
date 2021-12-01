c9230e1041a7910aeda6b90701f8f5c701c33b03
klemle2010 <54246142+klemle2010@users.noreply.github.com>
2021-12-01 14:46:38 -0500

    Day 1

---
+++ Day 1/index.js
@@ -0,0 +1,47 @@
+var fs = require('fs');
+var path = require('path');
+
+function GetData()
+{
+    try {
+        const data = fs.readFileSync('input.txt', 'utf8')
+        return data.split(/\r?\n/);
+      } catch (err) {
+        console.error(err)
+      }
+}
+
+function Part1()
+{
+    var inputData = GetData().map(x => Number(x));
+    var prevInput = null;
+    var increaseCounter = 0;
+
+    inputData.forEach(item => {
+        if(prevInput !== null && item > prevInput)
+        {
+            increaseCounter++;
+        }
+        prevInput = item;
+    });
+    console.log(`The number of increases in the file of length: ${inputData.length} was: ${increaseCounter}`);
+}
+
+function Part2()
+{
+    var inputData = GetData().map(x => Number(x));
+    var increaseCounter = 0;
+    var windowSize = 3;
+
+    for(var i = windowSize; i < inputData.length; i++)
+    {
+        if(inputData[i] > inputData[i - windowSize])
+        {
+            increaseCounter++;
+        }
+    }
+    console.log(`With a window size of ${windowSize}, the number of sliding window increases in the file of length: ${inputData.length} was: ${increaseCounter}`);
+}
+
+Part1();
+Part2();
